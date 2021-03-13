7175e74a0af746a9bfd654806facb85bd302f85f
Shintaro Katafuchi <hotchemi@users.noreply.github.com>
2021-03-13 14:50:29 +0900

    Update scre.rb

--- Casks/scre.rb
+++ Casks/scre.rb
@@ -1,6 +1,6 @@
 cask "scre" do
   version "0.2"
-  sha256 "ac27f9cd84d78bfa48000504b403d452d6504dde430cace782ccbc4baf338d5b"
+  sha256 "70739c781320d9c0dd3774f92ee95bc26ac9cdbe8e03e5c9d07105a4db9c02a8"
   url "https://github.com/hotchemi/Scre/releases/download/#{version}/Scre.zip"
   name "Scre"
   desc "A lightweight screen recorder macOS application written in SwiftUI."
